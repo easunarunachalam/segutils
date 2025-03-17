ec4f4b3a9a1a97e4cdd3dc3d1b24cb25249399b4
easunarunachalam <45492958+easunarunachalam@users.noreply.github.com>
2025-03-17 14:26:44 -0400

    require input to mitograph export to be precleaned

--- segutils/mitograph.py
+++ segutils/mitograph.py
@@ -5,7 +5,7 @@ import imageio as iio
 
 import matplotlib.pyplot as plt
 
-from .segmentation import get_bounding_box
+from .mask_utils import get_bounding_box
 
 def export_stacks_for_mitograph_single_timepoint(labels_2d_img, da_cyto, da_mito, min_size_threshold=50, min_dim_threshold=10, bg_intensity=99, scene=0, dry_run=True):
     
@@ -23,48 +23,48 @@ def export_stacks_for_mitograph_single_timepoint(labels_2d_img, da_cyto, da_mito
         # plt.imshow(is_curr_icell)
         # plt.show()
 
-        try:
-            y_bb, x_bb = get_bounding_box(is_curr_icell)
-            curr_bb_mask = is_curr_icell[y_bb,x_bb]
+        # try:
+        y_bb, x_bb = get_bounding_box(is_curr_icell)
+        curr_bb_mask = is_curr_icell[y_bb,x_bb]
+        
+        if not dry_run:
             
-            if not dry_run:
-                
-                # save network image stacks
-                curr_bb_mitostack = da_mito.data[:, y_bb, x_bb]
-                curr_bb_masked_mitostack = np.multiply(
-                    np.tile(curr_bb_mask, (curr_bb_mitostack.shape[0],1,1)),
-                    curr_bb_mitostack
-                )
-                background = np.random.randn(*(curr_bb_masked_mitostack.shape))*2 + bg_intensity
-                curr_bb_masked_mitostack[curr_bb_masked_mitostack == 0] = background[curr_bb_masked_mitostack == 0]
-        
-                if (curr_bb_masked_mitostack.shape[1] < min_dim_threshold) or (curr_bb_masked_mitostack.shape[2] < min_dim_threshold):
-                    continue
-        
-                tif_path = f"extracted_networks/S={scene:d}/S={scene:d}_cell={ilabel:d}.tif"
-                Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
-                iio.volwrite(tif_path, curr_bb_masked_mitostack)
-        
-        
-                # save masked cell stacks
-                curr_bb_cytostack = da_cyto.data[:, y_bb, x_bb]
-                curr_bb_masked_cytostack = np.multiply(
-                    np.tile(curr_bb_mask, (curr_bb_cytostack.shape[0],1,1)),
-                    curr_bb_cytostack
-                )
-        
-                tif_path = f"extracted_cells/S={scene:d}/S={scene:d}_cell={ilabel:d}_maskpxonly.tif"
-                Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
-                iio.volwrite(tif_path, curr_bb_masked_cytostack)
+            # save network image stacks
+            curr_bb_mitostack = da_mito.data[:, y_bb, x_bb]
+            curr_bb_masked_mitostack = np.multiply(
+                np.tile(curr_bb_mask, (curr_bb_mitostack.shape[0],1,1)),
+                curr_bb_mitostack
+            )
+            background = np.random.randn(*(curr_bb_masked_mitostack.shape))*2 + bg_intensity
+            curr_bb_masked_mitostack[curr_bb_masked_mitostack == 0] = background[curr_bb_masked_mitostack == 0]
+    
+            if (curr_bb_masked_mitostack.shape[1] < min_dim_threshold) or (curr_bb_masked_mitostack.shape[2] < min_dim_threshold):
+                continue
+    
+            tif_path = f"extracted_networks/S={scene:d}/S={scene:d}_cell={ilabel:d}.tif"
+            Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
+            iio.volwrite(tif_path, curr_bb_masked_mitostack)
+    
+    
+            # save masked cell stacks
+            curr_bb_cytostack = da_cyto.data[:, y_bb, x_bb]
+            curr_bb_masked_cytostack = np.multiply(
+                np.tile(curr_bb_mask, (curr_bb_cytostack.shape[0],1,1)),
+                curr_bb_cytostack
+            )
+    
+            tif_path = f"extracted_cells/S={scene:d}/S={scene:d}_cell={ilabel:d}_maskpxonly.tif"
+            Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
+            iio.volwrite(tif_path, curr_bb_masked_cytostack)
 
-                
-                # save cell image stacks
-                tif_path = f"extracted_cells/S={scene:d}/S={scene:d}_cell={ilabel:d}.tif"
-                Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
-                iio.volwrite(tif_path, curr_bb_cytostack)
             
-            n_images += 1
-        except:
-            continue
+            # save cell image stacks
+            tif_path = f"extracted_cells/S={scene:d}/S={scene:d}_cell={ilabel:d}.tif"
+            Path(tif_path).parent.mkdir(parents=True, exist_ok=True)
+            iio.volwrite(tif_path, curr_bb_cytostack)
+        
+        n_images += 1
+        # except:
+        #     continue
         
     return n_images
